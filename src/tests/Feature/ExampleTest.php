cc6f8190dd6b9bf148801dca01f384a6d5ab67fc
HIRO <stepiko135@gmail.com>
2020-08-19 19:42:09 +0900

    test:テスト追加

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -14,8 +14,33 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $this->get('/')->assertOk();
+        $this->post('/login', [
+            'email'    => 'test2@test.com',
+            'password' => 'password'
+        ]);
+        $this->get('/home')->assertStatus(200);
 
-        $response->assertStatus(200);
+        $this->get('/profile')->assertStatus(200);
+        $this->get('/profile/ヒロ/edit')->assertStatus(200);
+        $this->put('/profile/ヒロ/',[
+            'profile' => 'Test Profile Update'
+        ])->assertStatus(302);
+
+        $this->get('/routine')->assertStatus(405);
+        $this->get('/routine/1')->assertStatus(200);
+        $this->post('/routine', [
+            'user_id' => 2,
+            'name' => 'Testルーティン',
+            'desc' => 'Testルーティンです。'
+        ])->assertStatus(302);
+        $this->patch('/routine/2', [
+            'id' => 2,
+            'user_id' => 2,
+            'name' => 'Testルーティンアップデート',
+            'desc' => 'Testルーティンアップデートです。'
+        ])->assertStatus(302);
+        $this->delete('/routine/1')->assertStatus(302);
+
     }
 }
